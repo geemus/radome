238c2e5ceb19a456d5a363682e5e3d10dfcee2d2
geemus <geemus@gmail.com>
2010-06-21 13:16:04 -0700

    initial commit

---
+++ lib/radome.rb
@@ -0,0 +1,13 @@
+__DIR__ = File.dirname(__FILE__)
+
+$LOAD_PATH.unshift __DIR__ unless
+  $LOAD_PATH.include?(__DIR__) ||
+  $LOAD_PATH.include?(File.expand_path(__DIR__))
+  
+module Radome
+
+  unless const_defined?(:VERSION)
+    VERSION = '0.0.0'
+  end
+
+end
