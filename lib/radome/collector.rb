e29c1d9d37679d72769c553fc4529cf8696c6752
geemus <geemus@gmail.com>
2010-06-23 15:03:36 -0700

    rough first pass at a sensor and playing with integrating

--- lib/radome/collector.rb
+++ lib/radome/collector.rb
@@ -13,17 +13,18 @@ module Radome
     before { content_type "application/json" }
 
     get "/" do
-      content_type :json
       Thread.main[:data].to_json
     end
 
     put "/" do
       data = JSON.parse(request.body.read)
-      Thread.main[:data].merge!(data)
+      for key, value in data
+        Thread.main[:data][key] ||= {}
+        Thread.main[:data][key].merge!({Time.now => value})
+      end
       Thread.main[:data].to_json
     end
 
   end
-
 
 end
--- tests/helpers.rb
+++ tests/helpers.rb
@@ -33,11 +33,37 @@ def put_data(new_data)
   JSON.parse(data)
 end
 
+def sense
+  uname = `uname -mnv`
+  hostname, os, architecture = uname.match(/^(\S*) (.*) (.*)$/).captures
+
+  boottime = `sysctl kern.boottime`
+  seconds, microseconds = boottime.match(/\{ sec = ([0-9]*), usec = ([0-9]*) \}/).captures
+  system_boot_time = Time.at(seconds.to_i, microseconds.to_i).to_i
+
+  uptime = `uptime`
+  system_time = uptime.match(/^([\:0-9]*) /).captures.first
+  one_minute_load, five_minute_load, fifteen_minute_load = uptime.match(/load averages: ([\.0-9]*) ([\.0-9]*) ([\.0-9]*)/).captures
+
+  data = {
+    hostname => {
+      :architecture     => architecture,
+      :load => {
+        :one_minute     => one_minute_load.to_f,
+        :five_minute    => five_minute_load.to_f,
+        :fifteen_minute => fifteen_minute_load.to_f
+      },
+      :os               => os,
+      :system_boot_time => system_boot_time,
+      :system_time      => system_time
+    }
+  }
+end
+
 with_collector do
 
-  p get_data
-  p put_data({'a' => 'b'})
-  p put_data({'c' => 'd'})
-  p put_data({'a' => 'c'})
+  p put_data(sense)
+  sleep(1)
+  p put_data(sense)
 
 end
